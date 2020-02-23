9fdb340748134c9a23aa34b9b5228edb062837cf
maciekkkkkkk <mpeciakk@gmail.com>
2020-02-23 21:23:52 +0100

    Jumping arrow and project's style

--- js/index.js
+++ js/index.js
@@ -6,3 +6,26 @@ jumperDiv.addEventListener('click', () => {
         behavior: 'smooth'
     });
 });
+
+const projects = document.querySelectorAll('.project');
+const holders = document.querySelectorAll('.project-holder');
+
+for (let i = 0; i < projects.length; i++) {
+    const project = projects[i];
+    const holder = holders[i];
+
+    holder.addEventListener("mousemove", (event) => {
+        const rect = project.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        const yRotation = (x - project.clientWidth / 2) / 75;
+        const xRotation = -(y - project.clientHeight / 2) / 75;
+
+        project.style = `transform: rotateY(${yRotation}deg) rotateX(${xRotation}deg)`;
+    });
+
+    holder.addEventListener("mouseleave", () => {
+        project.style = `transition: transform 0.5s; transform: rotate(0)`;
+    });
+}
